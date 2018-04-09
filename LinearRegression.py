4276cc019029f999485986f36c9ae4fd4bb5ca84
Sumit Gupta <sumit743@gmail.com>
2018-04-08 20:16:28 -0400

    Simplified notation

--- LinearRegression.py
+++ LinearRegression.py
@@ -22,39 +22,40 @@ def plot(X, y, model):
     if X.shape[1] > 2:
         print "Cannot plot the 2D graph because training data is of higher dimension"
         return
-    print "Plotting linear regression...",
+    print "Plotting linear regression...\n"
 
     plt.scatter(X[:,1], y, marker=".")
 
     line = np.linspace(np.amin(X[:,1])-1, np.amax(X[:,1])+1)
     plt.plot(line, line * model[1] + model[0], color='red')
     plt.show()
-    print "Complete\n"
 
 def test(X_test, y_test, model):
-    print "\nTesting model..."
+    print "Testing model...\n"
     mean_squared_error = (1.0 / (2*X_test.shape[0])) * np.sum((np.dot(X_test, model) - y_test)**2)
 
     if VERBOSE:
-        print "Mean squared error on test data: " + str(mean_squared_error)
+        print "Mean squared error on test data: " + str(mean_squared_error) + "\n"
 
-    print "Complete\n"
     return mean_squared_error
 
 def train(X_train, y_train):
-    print "\nTraining model...",
+    print "Training model...\n"
 
-    parameters = np.zeros((X_train.shape[1], 1)) # initialize the parameters to 0
+    m = X_train.shape[1]
+
+    parameters = np.zeros((m, 1)) # initialize the parameters to 0
 
     # run gradient descent for ITERATIONS
     for i in range(ITERATIONS):
-        parameters = parameters - (ALPHA/DATASET_SIZE) * np.dot(X_train.transpose(), (np.dot(X_train, parameters) - y_train))
+        error = np.dot(X_train, parameters) - y_train # X*theta - y
+        partial_derivative = np.dot(X_train.transpose(), error) # X' * (X*theta - y)
+        parameters = parameters - (ALPHA/DATASET_SIZE) * partial_derivative
 
-    print "Completed \n"
-    return parameters
+    return parameters # this is the learned model 
 
 def generate_data():
-    print "Generating data...",
+    print "Generating data...\n"
 
     X, y = sklearn.datasets.make_regression(n_samples=DATASET_SIZE, n_features=NUMBER_OF_FEATURES, n_targets=NUMBER_OF_TARGETS, bias=BIAS, noise=NOISE) # generate regression data
 
@@ -62,7 +63,6 @@ def generate_data():
     X = np.concatenate((vector_of_ones, X), axis=1) # add a column of 1's in features
     y = y.reshape((y.shape[0], 1)) # reshape y e.g. from (10,) to (10,1)
 
-    print "Complete \n"
     return sklearn.model_selection.train_test_split(X, y, test_size=TEST_SIZE) # split the dataset into training / testing
 
 def start_regression():
@@ -77,6 +77,7 @@ def start_regression():
         print X_train[0:5,:] if X_train.shape[0] > 5 else X_train
         print "\nSome y_train data: "
         print y_train[0:5,:] if y_train.shape[0] > 5 else y_train
+        print "\n"
 
     model = train(X_train, y_train) # train the model
     mean_squared_error = test(X_test, y_test, model)
